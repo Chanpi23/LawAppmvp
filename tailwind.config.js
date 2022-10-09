67c9ded97c31839aef10fc9c836a8a460dab105e
chanpi23 <channycodez@gmail.com>
2022-10-08 23:36:42 -0400

    added in tail wind/ daisyUi

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{html,js,ejs}"],
+  theme: {
+    extend: {
+      colors: {}
+    },
+  },
+  plugins: [require("daisyUi")],
+
+  daisyUi:{
+    themes:["retro","dark"]
+  },
+}
